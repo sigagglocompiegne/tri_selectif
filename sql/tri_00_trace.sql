8f6ef71d817b104f3b0c0978f8c56517fc31424c
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-07 16:33:30 +0100

    Update tri_00_trace.sql

--- sql/tri_00_trace.sql
+++ sql/tri_00_trace.sql
@@ -1,1 +1,20 @@
+/*TRI SELECTIF V1.0*/
+/*Creation du fichier trace qui permet de suivre l'évolution du code*/
+/* tri_10_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : FGrégory Bodet */
+
+/*  
+  dépendances : 
+  
+  Liste des dépendances :
+  schéma          | table                 | description                                                   | usage
+
+*/
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+2018-12-07 : GB / initialisation du code SQL sur GitHub
 
