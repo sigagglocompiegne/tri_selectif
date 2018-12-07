e7a8b94244cbdd2e873b35d96150435b61e36730
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-07 16:38:11 +0100

    Update tri_20_seq.sql

--- sql/tri_20_seq.sql
+++ sql/tri_20_seq.sql
@@ -1,1 +1,85 @@
+/*TRI SELECTIF V1.0*/
+/*Creation des séquences intervenant dans l'incrémentation de certaines variables */
+/* tri_20_seq.sql */
+/*PostGIS*/
 
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
+DROP SEQUENCE IF EXISTS m_dechet.an_dec_pav_doc_media_gid_seq;
+DROP SEQUENCE IF EXISTS m_dechet.geo_dec_dechetterie_gid_seq;
+DROP SEQUENCE mIF EXISTS _dechet.geo_dec_pav_idconv_seq;
+DROP SEQUENCE IF EXISTS m_dechet.geo_dec_secteur_enc_seq;
+DROP SEQUENCE IF EXISTS m_dechet.geo_dec_secteur_om_seq;
+DROP SEQUENCE IF EXISTS m_dechet.log_dec_pav_gid_seq;
+
+-- ################################################################# Séquence sur TABLE  ###############################################
+
+-- Sequence: m_dechet.an_dec_pav_doc_media_gid_seq
+
+-- DROP SEQUENCE m_dechet.an_dec_pav_doc_media_gid_seq;
+
+CREATE SEQUENCE m_dechet.an_dec_pav_doc_media_gid_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 331
+  CACHE 1;
+  
+  -- Sequence: m_dechet.geo_dec_dechetterie_gid_seq
+
+-- DROP SEQUENCE m_dechet.geo_dec_dechetterie_gid_seq;
+
+CREATE SEQUENCE m_dechet.geo_dec_dechetterie_gid_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 4
+  CACHE 1;
+  
+  -- Sequence: m_dechet.geo_dec_pav_idconv_seq
+
+-- DROP SEQUENCE m_dechet.geo_dec_pav_idconv_seq;
+
+CREATE SEQUENCE m_dechet.geo_dec_pav_idconv_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 415
+  CACHE 1;
+  
+  -- Sequence: m_dechet.geo_dec_secteur_enc_seq
+
+-- DROP SEQUENCE m_dechet.geo_dec_secteur_enc_seq;
+
+CREATE SEQUENCE m_dechet.geo_dec_secteur_enc_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 838
+  CACHE 1;
+  
+  
+  -- Sequence: m_dechet.geo_dec_secteur_om_seq
+
+-- DROP SEQUENCE m_dechet.geo_dec_secteur_om_seq;
+
+CREATE SEQUENCE m_dechet.geo_dec_secteur_om_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 23
+  CACHE 1;
+  
+  -- Sequence: m_dechet.log_dec_pav_gid_seq
+
+-- DROP SEQUENCE m_dechet.log_dec_pav_gid_seq;
+
+CREATE SEQUENCE m_dechet.log_dec_pav_gid_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 32
+  CACHE 1;
+  
+  
