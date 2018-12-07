188cb0b56f819966db1e58b8b42f6a8c9124718b
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-07 16:34:30 +0100

    Update tri_10_schema.sql

--- sql/tri_10_schema.sql
+++ sql/tri_10_schema.sql
@@ -1,1 +1,20 @@
+/*TRI SELECTIF V1.0*/
+/*Creation du schéma qui va accueillir la structure des données */
+/* tri_10_schema.sql */
+/*PostGIS*/
 
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
+
+-- #################################################################### SCHEMA  ####################################################################
+
+-- Schema: m_dechet
+
+-- DROP SCHEMA m_dechet;
+
+CREATE SCHEMA m_dechet
+  AUTHORIZATION sig_create;
+  
+COMMENT ON SCHEMA m_dechet
+  IS 'Données métiers sur le thème des déchets et du recyclage';
