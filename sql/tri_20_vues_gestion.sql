403b7f39bbbd973938f33e9e17dbc56360a29f8b
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-17 16:50:08 +0100

    Update tri_20_vues_gestion.sql

--- sql/tri_20_vues_gestion.sql
+++ sql/tri_20_vues_gestion.sql
@@ -1,1 +1,9 @@
+/*TRI SELECTIF V1.0*/
+/*Creation du squelette de la structure des données */
+/* tri_20_vues_gestion.sql */
+/*PostGIS*/
 
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
+Sans objet
