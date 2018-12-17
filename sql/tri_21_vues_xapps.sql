e276b131ffd1aa4bfe0166e5850758fdc65e7740
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-17 16:52:27 +0100

    Update tri_21_vues_xapps.sql

--- sql/tri_21_vues_xapps.sql
+++ sql/tri_21_vues_xapps.sql
@@ -6,4 +6,19 @@
 /* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
 /* Auteur : Grégory Bodet */
 
+/* -------------------------------------------------------- xapps_geo_v_pav_orient ------------------------------------------- */
 
+-- View: x_apps.xapps_geo_v_pav_orient
+
+-- DROP VIEW x_apps.xapps_geo_v_pav_orient;
+
+CREATE OR REPLACE VIEW x_apps.xapps_geo_v_pav_orient AS 
+ SELECT row_number() OVER () AS row_id,
+    st_makeline(a.geom, b.geom)::geometry(LineString,2154) AS geom
+   FROM m_dechet.geo_dec_pav_verre a,
+    m_dechet.geo_dec_pav_verre b
+  WHERE a.id_contver = b.id_parent;
+
+
+COMMENT ON VIEW x_apps.xapps_geo_v_pav_orient
+  IS 'Vue géométrique des liens entre PAV supprimer, déplacer et le nouvel emplacement';
