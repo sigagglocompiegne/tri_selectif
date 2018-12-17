a5c2453df204709f1aff20c4dd4352d516555789
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-17 16:58:13 +0100

    Update tri_23_vues_xopendata.sql

--- sql/tri_23_vues_xopendata.sql
+++ sql/tri_23_vues_xopendata.sql
@@ -1,1 +1,9 @@
+/*TRI SELECTIF V1.0*/
+/* Creation des vues applicatives publiques */
+/* tri_23_vues_xopendata.sql */
+/*PostGIS*/
 
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
+Sans objet
