342f469d4371a7b10a91f443db5bde436279ea99
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-17 16:51:33 +0100

    Update tri_21_vues_xapps.sql

--- sql/tri_21_vues_xapps.sql
+++ sql/tri_21_vues_xapps.sql
@@ -1,1 +1,9 @@
+/*TRI SELECTIF V1.0*/
+/* Creation des vues applicatives */
+/* tri_21_vues_xapps.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
 
