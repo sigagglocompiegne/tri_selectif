314e64d5a60b843c33f1648016dfde6e769d5bd1
gregbodet <37414914+gregbodet@users.noreply.github.com>
2020-09-17 16:28:16 +0200

    Update tri_21_vues_xapps.sql

--- sql/tri_21_vues_xapps.sql
+++ sql/tri_21_vues_xapps.sql
@@ -6,13 +6,13 @@
 /* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
 /* Auteur : Grégory Bodet */
 
-/* -------------------------------------------------------- xapps_geo_v_pav_lieu_orient ------------------------------------------- */
+/* -------------------------------------------------------- xapps_geo_v_dec_pav_lieu_orient ------------------------------------------- */
 
--- View: x_apps.xapps_geo_v_pav_lieu_orient
+-- View: x_apps.xapps_geo_v_dec_pav_lieu_orient
 
--- DROP VIEW x_apps.xapps_geo_v_pav_lieu_orient;
+-- DROP VIEW x_apps.xapps_geo_v_dec_pav_lieu_orient;
 
-CREATE OR REPLACE VIEW x_apps.xapps_geo_v_pav_lieu_orient
+CREATE OR REPLACE VIEW x_apps.xapps_geo_v_dec_pav_lieu_orient
  AS
  SELECT row_number() OVER () AS row_id,
     st_makeline(a.geom, b.geom)::geometry(LineString,2154) AS geom
@@ -20,6 +20,6 @@ CREATE OR REPLACE VIEW x_apps.xapps_geo_v_pav_lieu_orient
     m_dechet.geo_dec_pav_lieu b
   WHERE a.idlieu = b.idparent;
   
-  COMMENT ON VIEW x_apps.xapps_geo_v_pav_lieu_orient
+  COMMENT ON VIEW x_apps.xapps_geo_v_dec_pav_lieu_orient
     IS 'Vue géométrique des liens entre PAV supprimer, déplacer et le nouvel emplacement (en pré-production)';
 
