676adfefae45e0a1fa9ba6445fabea5d59e882c3
gregbodet <37414914+gregbodet@users.noreply.github.com>
2020-10-15 12:04:37 +0200

    Delete tri_20_vues_gestion.sql

--- sql/tri_20_vues_gestion.sql
+++
@@ -1,9 +0,0 @@
-/*TRI SELECTIF V1.0*/
-/* Creation des vues de gestion */
-/* tri_20_vues_gestion.sql */
-/*PostGIS*/
-
-/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
-/* Auteur : Grégory Bodet */
-
-Sans objet
